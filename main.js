c2b8ba39cf473d2629d1462df9254b2a95e8e0d8
Sugeeta-123 <157228044+Sugeeta-123@users.noreply.github.com>
2024-07-19 08:28:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,275 @@
+var synth = window.speechSynthesis;
+
+
+function a(){
+    document.getElementById("ig").src="a.jpg";
+    speakThis = new SpeechSynthesisUtterance("A for Apple"); 
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+
+
+
+
+function x(){
+    document.getElementById("ig").src="x.jpg";
+    var speakThis= new SpeechSynthesisUtterance("X For X'mas" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+
+
+function b(){
+    document.getElementById("ig").src="b.jpg";
+    var speakThis= new SpeechSynthesisUtterance("B For ball" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+
+
+function c(){
+    document.getElementById("ig").src="c.jpg";
+    var speakThis= new SpeechSynthesisUtterance("c For cat" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+function d(){
+    document.getElementById("ig").src="d.jpg";
+    var speakThis= new SpeechSynthesisUtterance("d For dog" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function e(){
+    document.getElementById("ig").src="e.jpg";
+    var speakThis= new SpeechSynthesisUtterance("e For elephant" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function f(){
+    document.getElementById("ig").src="f.jpg";
+    var speakThis= new SpeechSynthesisUtterance("f For fish" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function g(){
+    document.getElementById("ig").src="g.jpg";
+    var speakThis= new SpeechSynthesisUtterance("g For Goat" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function h(){
+    document.getElementById("ig").src="h.jpg";
+    var speakThis= new SpeechSynthesisUtterance("h For hen" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+
+function i(){
+    document.getElementById("ig").src="i.jpg";
+    var speakThis= new SpeechSynthesisUtterance("i For igloo" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function j(){
+    document.getElementById("ig").src="j.jpg";
+    var speakThis= new SpeechSynthesisUtterance("j For jar" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+
+function k(){
+    document.getElementById("ig").src="k.jpg";
+    var speakThis= new SpeechSynthesisUtterance("k for kite" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function l(){
+    document.getElementById("ig").src="L.jpg";
+    var speakThis= new SpeechSynthesisUtterance("l For lion" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+}
+
+function m(){
+    document.getElementById("ig").src="m.jpg";
+    var speakThis= new SpeechSynthesisUtterance("m For monkey" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function n(){
+    document.getElementById("ig").src="n.jpg";
+    var speakThis= new SpeechSynthesisUtterance("n For net" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+
+function o(){
+    document.getElementById("ig").src="o.jpg";
+    var speakThis= new SpeechSynthesisUtterance("o For orange" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function p(){
+    document.getElementById("ig").src="p.jpg";
+    var speakThis= new SpeechSynthesisUtterance("p For pig" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function q(){
+    document.getElementById("ig").src="q.jpg";
+    var speakThis= new SpeechSynthesisUtterance("q For queen" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function r(){
+    document.getElementById("ig").src="r.jpg";
+    var speakThis= new SpeechSynthesisUtterance("r For rabbit" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function s(){
+    document.getElementById("ig").src="s.jpg";
+    var speakThis= new SpeechSynthesisUtterance("s For sun" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+
+function t(){
+    document.getElementById("ig").src="t.jpg";
+    var speakThis= new SpeechSynthesisUtterance("t For table" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+
+
+function u(){
+    document.getElementById("ig").src="u.jpg";
+    var speakThis= new SpeechSynthesisUtterance("u For umbrella" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function v(){
+    document.getElementById("ig").src="v.jpg";
+    var speakThis= new SpeechSynthesisUtterance("v For van" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function w(){
+    document.getElementById("ig").src="w.jpg";
+    var speakThis= new SpeechSynthesisUtterance("w For wheel" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+
+function y(){
+    document.getElementById("ig").src="y.jpg";
+    var speakThis= new SpeechSynthesisUtterance(" y For yak" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+function z(){
+    document.getElementById("ig").src="z.jpg";
+    var speakThis= new SpeechSynthesisUtterance("z For zebra" );
+    var voices = speechSynthesis.getVoices();
+    speakThis.voice = voices[10];
+    speakThis.rate = 0.7;
+    synth.speak(speakThis);
+
+}
+
+var check = ""
+
+function inp(){
+  
+    check = (document.getElementById("ip").value).toString().toLowerCase();
+    
+    console.log(check)
+    if(check.length == 1){
+        var functionName=window[check]
+        functionName();
+    }
+    else{
+        alert("Only type one alphabet at a time !")
+    }
+    document.getElementById('ip').value=""
+    
+
+  
+
+
+    
+
+}
